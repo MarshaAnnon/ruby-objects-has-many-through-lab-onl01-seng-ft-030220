5cdbd5b706d0f9ad5122fc920b0c7d93dff30ef1
Marsha Annon <marshaannon@gmail.com>
2020-03-19 14:09:35 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -24,7 +24,7 @@ class Doctor
   end
 
   def patients
-    Appointment.all.select {|appointment| patient.doctor == self}
+    Appointment.all.map {|appointment| appointment.patient}
   end
-  
+
 end
